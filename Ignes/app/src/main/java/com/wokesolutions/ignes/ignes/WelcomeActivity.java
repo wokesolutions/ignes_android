7e7ab70c63a9a1588ecf09bfd7e924ff4f7fa0bb
Catarina Matos <cp.matos@campus.fct.unl.pt>
2018-04-05 12:21:25 +0100

    apagar

--- Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/WelcomeActivity.java
+++ Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/WelcomeActivity.java
@@ -9,11 +9,5 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-<<<<<<< HEAD
-        //Estamos a fazer um teste
-=======
-
-        ////Catarina
->>>>>>> 0223a6201e543f653a2e3f8d9fb04b4d141d1a91
     }
 }

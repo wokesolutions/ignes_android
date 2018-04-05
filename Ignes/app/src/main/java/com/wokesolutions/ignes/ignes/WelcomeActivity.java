63f19962f5c76b7742d1790e928ce36aefd3c02c
Gonçalo Roxo <g.roxo@campus.fct.unl.pt>
2018-04-05 12:12:17 +0100

    Voltar as Origens
    Vamos começar

--- Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/WelcomeActivity.java
+++ Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/WelcomeActivity.java
@@ -8,7 +8,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome); //roxo teve aqui
-        setContentView(R.layout.activity_welcome); //roxo teve aqui
+        setContentView(R.layout.activity_welcome);
     }
 }

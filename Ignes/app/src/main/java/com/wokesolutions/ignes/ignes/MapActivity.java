2a69e42107cf13d65b181f865a2d76b160c034d3
Gonçalo Roxo <g.roxo@campus.fct.unl.pt>
2018-04-14 04:10:06 +0100

    fghj

--- Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/MapActivity.java
+++ Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/MapActivity.java
@@ -1,17 +1,10 @@
 package com.wokesolutions.ignes.ignes;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
 import android.location.Location;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -25,7 +18,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;

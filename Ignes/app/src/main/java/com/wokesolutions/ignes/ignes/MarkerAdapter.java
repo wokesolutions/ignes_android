7d384a694196d8b3088ea6b5fa49af21d1b6f7b8
Catarina Matos <cp.matos@campus.fct.unl.pt>
2018-05-16 18:41:54 +0100

    changes

--- Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/MarkerAdapter.java
+++ Ignes/app/src/main/java/com/wokesolutions/ignes/ignes/MarkerAdapter.java
@@ -2,14 +2,18 @@ package com.wokesolutions.ignes.ignes;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import java.util.Map;
 
 public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder> {
@@ -38,7 +42,7 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
 
-        Object[] keys =  mMap.keySet().toArray();
+        Object[] keys = mMap.keySet().toArray();
 
         final MarkerClass markerItem = mMap.get(keys[position]);
 
@@ -50,10 +54,11 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
         TextView gravity = holder.marker_gravity;
         TextView status = holder.marker_status;
         Button more_button = holder.button_more;
-        TextView likes = holder.marker_likes;
-        TextView dislikes = holder.marker_dislikes;
+        // TextView likes = holder.marker_likes;
+        // TextView dislikes = holder.marker_dislikes;
         TextView gravity_title = holder.marker_gravity_title;
-        // TextView comments = holder.marker_comments;
+        TextView interacts = holder.marker_interacts;
+      //  View layout = holder.marker_layout;
 
         if (!markerItem.getmTitle().isEmpty())
             title.setText(markerItem.getmTitle());
@@ -70,11 +75,23 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
             gravity.setVisibility(View.GONE);
             gravity_title.setVisibility(View.GONE);
         }
+      /*  if (markerItem.getmGravity().equals("1"))
+            layout.setBackgroundColor(Color.parseColor("#54A562"));
+        if (markerItem.getmGravity().equals("2"))
+            layout.setBackgroundColor(Color.parseColor("#368043"));
+        if (markerItem.getmGravity().equals("3"))
+            layout.setBackgroundColor(Color.parseColor("#1A5023"));
+        if (markerItem.getmGravity().equals("4"))
+            layout.setBackgroundColor(Color.parseColor("#0F3A16"));
+        if (markerItem.getmGravity().equals("5"))
+            layout.setBackgroundColor(Color.parseColor("#0F3A16"));*/
+
 
         status.setText(markerItem.getmStatus());
-        likes.setText(markerItem.getmLikes());
-        dislikes.setText(markerItem.getmDislikes());
-        //  comments.setText(markerItem.getmComments());
+        // likes.setText(markerItem.getmLikes());
+        // dislikes.setText(markerItem.getmDislikes());
+        int total = Integer.parseInt(markerItem.getmLikes()) + Integer.parseInt(markerItem.getmDislikes());
+        interacts.setText("" + total);
 
         image.setImageBitmap(markerItem.getmImg_bitmap());
 
@@ -110,7 +127,7 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
         TextView marker_title;
         TextView marker_likes;
         TextView marker_dislikes;
-        //TextView marker_comments;
+        TextView marker_interacts;
         TextView marker_address;
         TextView marker_username;
         TextView marker_date;
@@ -119,6 +136,7 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
         TextView marker_gravity_title;
         Button button_more;
         Context itemViewContext;
+     //   View marker_layout;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -131,10 +149,11 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
             marker_gravity = itemView.findViewById(R.id.feed_gravity_marker);
             marker_status = itemView.findViewById(R.id.feed_status_marker);
             button_more = itemView.findViewById(R.id.button_more);
-            marker_likes = itemView.findViewById(R.id.feed_likes_number);
-            marker_dislikes = itemView.findViewById(R.id.feed_dislikes_number);
+            //  marker_likes = itemView.findViewById(R.id.feed_likes_number);
+            // marker_dislikes = itemView.findViewById(R.id.feed_dislikes_number);
             marker_gravity_title = itemView.findViewById(R.id.feed_gravity_title);
-            // marker_comments = itemView.findViewById(R.id.feed_comments_number);
+            marker_interacts = itemView.findViewById(R.id.feed_total_number);
+          //  marker_layout = itemView.findViewById(R.id.layout_marker_feed);
 
         }
     }
